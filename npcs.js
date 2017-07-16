3712354bd5773daf92b763c8ae7a6fa7d6cbce88
Darren Kearney <github@darrenk.net>
2017-07-16 12:57:25 +0100

    Added npcs.js - json of some npc objects with some examples. For now is good for a quick and dirty 'base' though I imagine it would be better to seperate it out into Classes or Types of npc based on defining properties.

---
+++ npcs.js
@@ -0,0 +1,45 @@
+{
+    [
+        'goblin':{
+            'name':'Goblin',
+            'type':'GOBLIN',
+            'class':'fighter',
+            'hp': 3,
+            'hpMax': 3,
+            'damage': 1,
+            'xp': 1,
+            'talkText': "GRrrurumblor"
+            },
+        'sorceress': {
+            'name':'Julior',
+            'type':'HUMAN',
+            'class':'sorcerer',
+            'hp': 5,
+            'hpMax': 5,
+            'damage': 2,
+            'xp': 2,
+            'talkText': "Kazam!"
+        },
+        'ogre': {
+            'name':'Bash',
+            'type':'GOBLIN',
+            'class':'fighter',
+            'hp': 6,
+            'hpMax': 6,
+            'damage': 2,
+            'xp': 2,
+            'talkText': "Roaugh!"
+        },
+        'archer': {
+            'name':'Robyn',
+            'type':'HUMAN',
+            'class':'ranger',
+            'hp': 4,
+            'hpMax': 4,
+            'damage': 1,
+            'xp': 3,
+            'talkText': "Roaugh!"
+        }
+    ]
+}
+
